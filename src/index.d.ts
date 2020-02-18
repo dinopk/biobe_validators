c470791baf06add7d3a00c6666aa240d10080e08
Victor Marques <v.marques@AVIVATEC.COM.BR>
2020-02-18 13:56:33 -0300

    validators

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Retorna true se o CNPJ estiver válido
+ * @param cnpj Cadastro Nacional da Pessoa Jurídica
+ */
+export function isCnpj(cnpj: string): boolean;
+
+/**
+ * Retorna true se o CPF estiver válido
+ * @param cpf Cadastro de Pessoas Físicas
+ */
+export function isCpf(cpf: string): boolean;
